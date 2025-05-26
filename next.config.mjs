3c7a1973a771e2274c208b1c2815ba2141472c34
ziadmustafa1 <123ziadmstfy@gmail.com>
2025-05-27 02:47:35 +0300

    Update various components, scripts, and configurations to enhance functionality and fix issues. This includes improvements to admin actions, user management, and database scripts, as well as updates to UI components and styles. Additionally, several migration scripts have been added or modified to ensure database integrity and performance.

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,17 @@ const nextConfig = {
     unoptimized: true,
   },
   reactStrictMode: true,
+  onDemandEntries: {
+    maxInactiveAge: 60 * 60 * 1000,
+    pagesBufferLength: 5,
+  },
+  webpack: (config, { isServer }) => {
+    config.ignoreWarnings = [
+      { module: /node_modules/ },
+      { message: /Critical dependency/ },
+    ];
+    return config;
+  },
   env: {
     NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
   },
